28a30aaff171c2409787d8d830a12ce8a56452ec
Chiara Campalani <campalanichiara@gmail.com>
2021-01-13 17:06:39 +0900

    completed exercise

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
 
   validates :restaurant, presence: true
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: true
+  RATING = [0, 1, 2, 3, 4, 5]
+  validates :rating, presence: true, inclusion: { in: RATING }, numericality: true
 end
